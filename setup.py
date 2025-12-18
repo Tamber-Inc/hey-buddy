57ef89a0a158750318983e42bd0e08f9007dbf4a
Jamie Pond <jamiepond259@gmail.com>
2025-12-18 04:05:33 -0800

    chore: bump version to 0.2.15

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ extra_deps = {
 setup(
     name="heybuddy",
     description="Hey Buddy is a tool for training wake-word-detecting neural networks for use in web browsers.",
-    version="0.2.14",
+    version="0.2.15",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     license="Apache 2.0",
